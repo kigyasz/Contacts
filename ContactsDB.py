e2624fdc50f5eac73f653c4657364a16894aecd0
MIB <amigacc@freemail.hu>
2023-02-13 00:39:42 +0100

    V0.00.000

---
+++ ContactsDB.py
@@ -0,0 +1,21 @@
+# Contacts Database Manager
+# Start Datze: 2023.02.12
+
+# imports
+import sqlite3
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+
+# check database
+
+def load_vcard2db(vcad_file):
+    pass
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.show()
+sys.exit(app.exec())
+
+
+
+
